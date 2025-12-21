3483df8875d5c9da5fe1e87327583a02748e6227
lippard <59750276+lippard661@users.noreply.github.com>
2025-12-21 08:09:44 -0700

    Add files via upload
    Workaround longstanding macOS issue with sa.

--- rotateacct.sh
+++ rotateacct.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # 2025-12-13: Stolen from OpenBSD /etc/daily.
+# 2025-12-21: Modified to workaround macOS sa -sq bug (it fails
+#   on second and subsequent runs before truncating acct).
 # Rotate process accounting logs.
 
 if [ -f /var/account/acct ]; then
@@ -11,6 +13,9 @@ if [ -f /var/account/acct ]; then
     test -f /var/account/acct.0 && \
 	/bin/mv -f /var/account/acct.0 /var/account/acct.1
     /bin/cp -f /var/account/acct /var/account/acct.0
+    # Work around longstanding bug in sa reported in 2012.
+    # https://discussions.apple.com/thread/3639471?sortBy=rank
+    test -f /var/account/usracct && /bin/rm -rf /var/account/usracct
     /usr/sbin/sa -sq
     /usr/bin/lastcomm -f /var/account/acct.0 | /usr/bin/grep -e ' -[A-Z]*[EMPTU]'
 fi
